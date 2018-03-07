49cf22c0655cfe2a922ce7c72ce365b577c51ff1
Erul <erulggubas@yahoo.com>
2018-03-07 15:35:07 +0800

    enabled Authentication and edited timeline

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,11 @@
 export const environment = {
     production: false,
-    var config = {
-    apiKey: "AIzaSyCziGUWBbh0b7tgIKbuhJ1q4xV9v7E1Pp0",
+    firebaseConfig: {
+        apiKey: "AIzaSyCziGUWBbh0b7tgIKbuhJ1q4xV9v7E1Pp0",
     authDomain: "firestoat-ecac0.firebaseapp.com",
     databaseURL: "https://firestoat-ecac0.firebaseio.com",
     projectId: "firestoat-ecac0",
     storageBucket: "firestoat-ecac0.appspot.com",
     messagingSenderId: "104314963530"
-  };
+    }
+};
