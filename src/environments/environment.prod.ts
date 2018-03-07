c4868f204360d41cac14495b12845406b32f6c2e
Erul <erulggubas@yahoo.com>
2018-03-08 00:53:17 +0800

    modified firebase and deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,12 @@
 
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyCziGUWBbh0b7tgIKbuhJ1q4xV9v7E1Pp0",
+    authDomain: "firestoat-ecac0.firebaseapp.com",
+    databaseURL: "https://firestoat-ecac0.firebaseio.com",
+    projectId: "firestoat-ecac0",
+    storageBucket: "firestoat-ecac0.appspot.com",
+    messagingSenderId: "104314963530"
+    }
+};
